b8dcaf4eea0e3616db20088046935d4bbed70b7f
Cloud Native <cloudnative168@gmail.com>
2024-10-12 22:27:06 +0100

    modify states

--- Infra-Code-Terraform/azure-hashicorp-vault-vm/backend.tf
+++ Infra-Code-Terraform/azure-hashicorp-vault-vm/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstates"
-    storage_account_name = "tfstatestorageaccont123"
+    storage_account_name = "tfstatefilestorageacount"
     container_name       = "tfstates"
     key                  = "End-to-End-Azure-Kubernetes-Three-Tier-Project/HashiCorp-Vault-Server/terraform.state"
   }
